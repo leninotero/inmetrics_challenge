603929ce36fba321aeb631f5344115252fe7dc75
Lenin Abadie <leninotero@users.noreply.github.com>
2020-10-29 22:36:14 -0300

    FIX - update hooks

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -2,7 +2,7 @@ Before do
     page.current_window.resize_to(1920, 1080)
 end
 
-After do
+After do |scenario|
     screenshot = page.save_screenshot("report/screenshots/#{scenario.__id__}.png")
     embed(screenshot, "image/png", "Screenshot")
 end
