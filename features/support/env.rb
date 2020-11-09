982ff33141e39f95153279ae1a580249df8ad9ff
Lenin Abadie <leninotero@users.noreply.github.com>
2020-11-08 23:08:32 -0300

    update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,9 +7,29 @@ require "httparty/request"
 require "httparty/response/headers"
 require "faker"
 
+case ENV["BROWSER"]
+when "firefox"
+  @driver = :selenium
+when "chrome"
+  @driver = :selenium_chrome
+when "headless"
+  Capybara.register_driver :selenium_chrome_headless do |app|
+    chrome_options = Selenium::WebDriver::Chrome::Options.new.tap do |options|
+      options.add_argument "--headless"
+      options.add_argument "--disable-gpu"
+      options.add_argument "--no-sandbox"
+      options.add_argument "--disable-site-isolation-trials"
+    end
+    Capybara::Selenium::Driver.new(app, browser: :chrome, options: chrome_options)
+  end
+  @driver = :selenium_chrome_headless
+else
+  puts "Invalid browser"
+end
+
 
 Capybara.configure do |config|
-  config.default_driver = :selenium_chrome
-  config.app_host = "http://www.inmrobo.tk/accounts/login"
-  
+  config.default_driver = @driver
+  config.default_max_wait_time = 10
+  config.app_host = "http://www.inmrobo.tk/accounts/login"  
 end
